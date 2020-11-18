70047153fcad52b32b944705ae69c519db02435a
unknown <alia.azarian@gmail.com>
2020-11-18 16:37:22 +0000

    Use the react-countup library to animate the api values from 0-value on app load and modify the lastUpdate prop using 'new Date' and 'toDateString' to turn it into a human-readable format

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+import CountUp from 'react-countup'
 
 import styles from './Cards.module.css'
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+
     if (!confirmed) {
         return "...Loading Data"
     }
@@ -14,8 +16,10 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">{confirmed.value}</Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={confirmed.value} duration={2.5} separator={','} />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
@@ -23,8 +27,10 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography variant="h5">{recovered.value}</Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={recovered.value} duration={2.5} separator={','} />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of recovered cases from COVID-19</Typography>
                     </CardContent>
                 </Grid>
@@ -32,8 +38,10 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                        <Typography variant="h5">{deaths.value}</Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={deaths.value} duration={2.5} separator={','} />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
                     </CardContent>
                 </Grid>
